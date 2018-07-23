ced6aa9506e55fd3d7adace2e32f00161cd27115
ILoveToDevelopApplications <chezzplayzz@yahoo.com>
2018-07-23 09:12:59 -0400

    lab 5

---
+++ Lab5GrandCircus/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5GrandCircus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Welcome to Factorial Calculator!");
+
+            Console.WriteLine("\n Enter a number 1-10");
+
+            long int  numberEnter = Console.ReadLine();
+        }
+
+        public static int getFactorial()
+        {
+            bool toContinue = true;
+            
+            while(!toContinue)
+            {
+
+
+
+
+
+                Console.WriteLine("Continue? Y/N\t ");
+
+                var  userInput = Console.Read.key().Read.Char;
+
+                
+                if( userInput == 'Y')
+                {
+                    toContinue = false;
+
+                    Console.WriteLine("See You Next Time!")
+                }
+
+
+            }
+
+
+        }
+    }
+}
---
+++ Lab5GrandCircus/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lab5GrandCircus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab5GrandCircus")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dd63a3d6-9395-44b5-95c8-c46fa022e297")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
